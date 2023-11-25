1d49a5b69621062700f1393c85351c219b939cbc
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-25 17:12:54 +0100

    add theme.js to the project

---
+++ src/theme.js
@@ -0,0 +1,37 @@
+export const theme = {
+    color: {
+        white: "#FFFFFF",
+        whiteLilac: "#FBFBFE",
+        mercury: "#E5E5E5",
+        iron: "#D1D5DA",
+        silver: "#C4C4C4",
+        slateGray: "#6E7E91",
+        dodgerBlue: "#2188FF",
+        scienceBlue: "#0366D6",
+        mineShaft: "#252525",
+    },
+
+    shadow: {
+        boxShadow: "0px 16px 58px 0px #090A3308",
+        boxShadow: "0px -2px 50px 0px #090A3305",
+    },
+
+    border: {
+        border: "6px solid #D1D5DA4D",
+    },
+
+    font: {
+        font12: "12px",
+        font14: "14px",
+        font16: "16px",
+        font18: "18px",
+        font20: "20px",
+        font20: "38px",
+    },
+
+    breakpoint: {
+        mobile: 768,
+        tablet: 992,
+        laptop: 1200,
+    },
+};
