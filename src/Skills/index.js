68c85970591f23209fc3cfd324c9c16187a3c154
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-26 18:57:36 +0100

    add custom listDotIcon to Skills component

--- src/Skills/index.js
+++ src/Skills/index.js
@@ -1,9 +1,12 @@
+import { ReactComponent as ListDotIcon } from './Ellipse31.svg';
+
 const Skills = ({ title, body }) => (
     <section>
         <header>{title}</header>
         <body>
             {body.map((skill, index) => (
                 <li key={index}>
+                    <ListDotIcon />
                     {skill}
                 </li>
             ))}
