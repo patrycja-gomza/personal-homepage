3c3af6cba66e79b46c84f3b74623aaaef23e1847
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-28 13:27:20 +0100

    fetch GitHub projects using axios

--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,7 +1,24 @@
+import axios from 'axios';
+import { useState, useEffect } from "react";
 import { ReactComponent as GitHubIcon } from './mark-github.svg';
 import personalData from '../personalData.json';
 
 const Portfolio = () => {
+    const [projects, setProjects] = useState();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(personalData.githubAPI_URL);
+                const data = response.data;
+
+                setProjects(data);
+            } catch (error) {
+                console.error('Error fetching GitHub projects:', error);
+            }
+        })();
+    }, []);
+
     return (
         <article>
             <header>
@@ -9,10 +26,32 @@ const Portfolio = () => {
                 <h2>Portfolio</h2>
                 <p>My recent projects</p>
             </header>
-
-            <body>
-                tiles - GitHub
-            </body>
+            <div>
+                {projects && projects.map((project) => (
+                    <div key={project.id}>
+                        <h3>{project.name}</h3>
+                        <p>{project.description}</p>
+                        <div>
+                            {project.homepage && (
+                                <>
+                                    Demo:{" "}
+                                    <a href={project.homepage} target="_blank" rel="noopener noreferrer">
+                                        {project.homepage}
+                                    </a>
+                                </>
+                            )}
+                            {project.html_url && (
+                                <>
+                                    Code:{" "}
+                                    <a href={project.html_url} target="_blank" rel="noopener noreferrer">
+                                        {project.html_url}
+                                    </a>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </article>
     );
 };
