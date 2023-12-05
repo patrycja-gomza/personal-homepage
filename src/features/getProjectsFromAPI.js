e7ff89a65e18f13e19fbd02ed31c6b8d46080317
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-05 15:24:55 +0100

    introduce getProjectsFromAPI function in new file

---
+++ src/features/getProjectsFromAPI.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import personalData from '../personalData.json';
+
+export const getProjectsFromAPI = async () => {
+    try {
+        const response = await axios.get(personalData.githubAPI_URL);
+        return response.data;
+    } catch (error) {
+        throw new Error('Error fetching GitHub projects:', error);
+    }
+};
