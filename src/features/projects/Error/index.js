aac3e736a6ea300f9d3bce3aefa2d101b84d940c
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-09 13:45:36 +0100

    introduce styled components for the Error

--- src/features/projects/Error/index.js
+++ src/features/projects/Error/index.js
@@ -1,13 +1,14 @@
 import { ReactComponent as ErrorIcon } from '../../../images/error-icon.svg';
+import { ErrorContainer, ErrorTitle, ErrorText, ErrorButton } from './styled';
 
 const Error = () => {
     return (
-        <div>
+        <ErrorContainer>
             <ErrorIcon />
-            <p>Ooops! Something went wrong...</p>
-            <span>Sorry, failed to load Github projects.<br />You can check them directly on Github.</span>
-            <button>Go to Github</button>
-        </div >
+            <ErrorTitle>Ooops! Something went wrong...</ErrorTitle>
+            <ErrorText>Sorry, failed to load Github projects.<br />You can check them directly on Github.</ErrorText>
+            <ErrorButton>Go to Github</ErrorButton>
+        </ErrorContainer >
     );
 };
 
