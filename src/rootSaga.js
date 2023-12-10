32c450d5815ee797f9e8e55b0a02e377bcc46012
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-10 12:49:28 +0100

    integrate modesSaga into rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import { projectsSaga } from "./features/projects/projectsSaga";
+import { modesSaga } from "./features/modes/modesSaga";
 
 export default function* rootSaga() {
     yield all([
         projectsSaga(),
+        modesSaga(),
     ])
 };
