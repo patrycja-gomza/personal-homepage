567678bdb951cf0ea0b59a6f9220086c9e1530dd
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-11 13:21:12 +0100

    rename handler and simplify state storage to localStorage

--- src/features/modes/modesSaga.js
+++ src/features/modes/modesSaga.js
@@ -1,21 +1,18 @@
-import { takeEvery, put, call, select } from "redux-saga/effects";
-import { toggleDarkMode, selectDarkMode } from "./modesSlice";
+import { takeEvery, call, select } from "redux-saga/effects";
+import { selectDarkMode } from "./modesSlice";
 import {
     saveDarkModePreferenceToLocalStorage,
-    loadDarkModePreferenceFromLocalStorage
 } from "./modesLocalStorage";
 
-function* toggleDarkModeHandler() {
+function* saveModeToLocalStorageHandler() {
     try {
-        const darkModePreference = yield call(loadDarkModePreferenceFromLocalStorage);
-        yield put(toggleDarkMode(darkModePreference));
         const currentDarkMode = yield select(selectDarkMode);
-        yield call(saveDarkModePreferenceToLocalStorage, !currentDarkMode);
+        yield call(saveDarkModePreferenceToLocalStorage, currentDarkMode);
     } catch (error) {
         console.error('Error handling dark mode:', error);
     }
 };
 
 export function* modesSaga() {
-    yield takeEvery(toggleDarkMode.type, toggleDarkModeHandler);
+    yield takeEvery("*", saveModeToLocalStorageHandler);
 };
