e035978cbe4dcd0931658da7604309eb18399de1
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-13 21:05:35 +0100

    add dark mode support to Header component

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,16 +1,20 @@
+import { useSelector } from 'react-redux';
 import profilePhoto from '../../images/pgomza.jpg';
 import personalData from '../../personalData.json';
 import { ReactComponent as EmailIcon } from '../../images/message.svg';
 import emojiWoman from "../../images/woman.svg";
+import emojiWomanDark from "../../images/woman-dark.svg";
 import emojiPc from "../../images/pc.svg";
 import { StyledHeader, StyledPhoto, Wrapper } from './styled';
 import { Intro, Title, Paragraph, Button } from '../../styles/sharedStyled';
+import { selectDarkMode } from '../../features/modes/modesSlice';
 
 const Header = () => {
     const name = personalData.name;
     const description = personalData.description;
     const email = personalData.email;
     const security = { target: '_blank', rel: 'noopener noreferrer' };
+    const darkMode = useSelector(selectDarkMode);
 
     return (
         <StyledHeader>
@@ -19,7 +23,10 @@ const Header = () => {
                 <Intro>This is</Intro>
                 <Title>{name}</Title>
                 <Paragraph>
-                    <img src={emojiWoman} alt="emoji" height="30px" style={{ verticalAlign: 'middle' }} />
+                    {darkMode ?
+                        <img src={emojiWomanDark} alt="emoji" height="30px" style={{ verticalAlign: 'middle' }} />
+                        :
+                        <img src={emojiWoman} alt="emoji" height="30px" style={{ verticalAlign: 'middle' }} />}
                     <img src={emojiPc} alt="emoji" height="30px" style={{ verticalAlign: 'middle' }} />
                     {" "}
                     {description}
