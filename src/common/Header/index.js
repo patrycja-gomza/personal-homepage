3a0d4749471a0b49f437efdbdb9a375da03aab6e
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-16 13:52:07 +0100

    remove unnecessary imports and simplify code

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,18 +1,14 @@
-import { useSelector } from 'react-redux';
 import profilePhoto from '../../images/pgomza.jpg';
 import { usePersonalData } from '../../data/usePersonalData';
 import { ReactComponent as EmailIcon } from '../../images/message.svg';
 import emojiWoman from "../../images/woman.svg";
-import emojiWomanDark from "../../images/woman-dark.svg";
 import emojiPc from "../../images/pc.svg";
 import { StyledHeader, StyledPhoto, Wrapper } from './styled';
 import { Intro, Title, Paragraph, Button } from '../../styles/sharedStyled';
-import { selectDarkMode } from '../../features/modes/modesSlice';
 
 const Header = () => {
     const { name, description, email } = usePersonalData();
     const security = { target: '_blank', rel: 'noopener noreferrer' };
-    const darkMode = useSelector(selectDarkMode);
     const emojiStyle = { verticalAlign: 'middle', height: '30px' };
 
     return (
@@ -22,7 +18,7 @@ const Header = () => {
                 <Intro>This is</Intro>
                 <Title>{name}</Title>
                 <Paragraph>
-                    <img src={darkMode ? emojiWomanDark : emojiWoman} alt="emoji" style={emojiStyle} />
+                    <img src= {emojiWoman} alt="emoji" style={emojiStyle} />
                     <img src={emojiPc} alt="emoji" style={emojiStyle} />
                     {" "}
                     {description}
