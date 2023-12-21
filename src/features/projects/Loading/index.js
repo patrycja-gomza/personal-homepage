340d3eb61b02c9c0e366249a7b673e50903887ef
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-12-21 14:44:16 +0100

    simplify the code for Loading

--- src/features/projects/Loading/index.js
+++ src/features/projects/Loading/index.js
@@ -1,13 +1,11 @@
 import { LoadingSpinner } from './styled';
 import { StatusContainer, StatusText } from '../styled';
 
-const Loading = () => {
-    return (
-        <StatusContainer>
-            <StatusText>Please wait, projects are being loaded...</StatusText>
-            <LoadingSpinner />
-        </StatusContainer>
-    );
-};
+const Loading = () => (
+    <StatusContainer>
+        <StatusText>Please wait, projects are being loaded...</StatusText>
+        <LoadingSpinner />
+    </StatusContainer>
+);
 
 export default Loading;
