29ac4673ef8f91204ab7f5b1c94808966e00fd6c
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-02-05 21:36:01 +0100

    enhanced routing in App, add Redirect for the default path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import ArtPage from "./ArtPage";
 import PersonalHomepage from "./PersonalHomepage";
+import { toArtPage, toHomepage } from "./routes";
 
 function App() {
     return (
         <HashRouter>
             <Switch>
-                <Route path="/art" component={ArtPage} />
-                <Route path="/" component={PersonalHomepage} />
+                <Route path={toArtPage()}>
+                    <ArtPage />
+                </Route>
+                <Route path={toHomepage()}>
+                    <PersonalHomepage />
+                </Route>
+                <Route path="/">
+                    <Redirect to={toHomepage()} />
+                </Route>
             </Switch>
         </HashRouter>
     );
