c7e933cea57e9122b223630a8ffc665c116c16fe
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-02-07 21:43:35 +0100

    optimize Header for ArtPage navigation and improved reusability

--- src/PersonalHomepage/Header/index.js
+++ src/PersonalHomepage/Header/index.js
@@ -1,22 +1,30 @@
-import { Link } from 'react-router-dom';
-import profilePhoto from '../images/pgomza.jpg';
+import { NavLink } from 'react-router-dom';
 import { usePersonalData } from '../usePersonalData';
 import { ReactComponent as EmailIcon } from '../images/message.svg';
 import emojiWoman from "../images/woman.svg";
 import emojiPc from "../images/pc.svg";
-import { StyledHeader, StyledPhoto, Wrapper } from './styled';
+import { StyledHeader, StyledPhoto, Wrapper, PhotoWrapper, TextOverlay } from './styled';
 import { Intro, Title, Paragraph, Button } from '../../sharedStyled';
 
-const Header = () => {
-    const { name, description, email } = usePersonalData();
+const Header = ({ to, photo, name, description, isArtPage }) => {
+    const { email } = usePersonalData();
     const security = { target: '_blank', rel: 'noopener noreferrer' };
     const emojiStyle = { verticalAlign: 'middle', height: '30px' };
 
     return (
         <StyledHeader>
-            <Link to="/art">
-                <StyledPhoto src={profilePhoto} alt="Patrycja Gomza" />
-            </Link>
+            <NavLink to={to} style={{ textDecoration: 'none' }}>
+                <PhotoWrapper>
+                    <StyledPhoto src={photo} alt="Patrycja Gomza" />
+                    <TextOverlay>
+                        {isArtPage ? 
+                        "Back to home page" 
+                        : 
+                        "Click here & discover my other side"
+                        }
+                    </TextOverlay>
+                </PhotoWrapper>
+            </NavLink>
             <Wrapper>
                 <Intro>This is</Intro>
                 <Title>{name}</Title>
