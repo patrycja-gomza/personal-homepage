c201420b1fe5988b75f1b5b56bf5660256037c73
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-03-02 20:16:19 +0100

    introduce modal window functionality in ArtSkills to display images in enlarged format

--- src/features/ArtPage/ArtSkills/index.js
+++ src/features/ArtPage/ArtSkills/index.js
@@ -1,7 +1,30 @@
 import { StyledSection, SectionBody, TileImage, TileImageContainer } from './styled';
 import { SectionHeader } from '../../../common/SectionHeader/SectionHeader';
+import ModalWindow from '../ModalWindow';
+import { useState } from 'react';
 
 const ArtSkills = ({ title, emoji, skills }) => {
+    const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = (index) => {
+        setSelectedImageIndex(index);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedImageIndex(null);
+        setIsModalOpen(false);
+    };
+
+    const goToPrevImage = () => {
+        setSelectedImageIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : skills.length - 1));
+    };
+
+    const goToNextImage = () => {
+        setSelectedImageIndex((prevIndex) => (prevIndex < skills.length - 1 ? prevIndex + 1 : 0));
+    };
+
     return (
         <StyledSection>
             <SectionHeader>
@@ -11,7 +34,10 @@ const ArtSkills = ({ title, emoji, skills }) => {
             </SectionHeader>
             <SectionBody>
                 {skills.map((skill, index) => (
-                    <TileImageContainer key={index}>
+                    <TileImageContainer
+                        key={index}
+                        onClick={() => openModal(index)}
+                    >
                         <TileImage
                             src={skill.url}
                             alt="artskills"
@@ -20,6 +46,15 @@ const ArtSkills = ({ title, emoji, skills }) => {
                     </TileImageContainer>
                 ))}
             </SectionBody>
+
+            {isModalOpen && (
+                <ModalWindow
+                    imageUrl={skills[selectedImageIndex].url}
+                    onClose={closeModal}
+                    onPrev={goToPrevImage}
+                    onNext={goToNextImage}
+                />
+            )}
         </StyledSection>
     );
 };
