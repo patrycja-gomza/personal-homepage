4f5da3597f4169df456186f73b28f1fb1330ffb7
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-03-02 20:21:14 +0100

    introduce styled components for ModalWindow

---
+++ src/features/ArtPage/ModalWindow/styled.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  transition: all 0.3s;
+`;
+
+export const ModalContent = styled.div`
+  position: relative;
+  max-width: 90%;
+  max-height: 90%;
+  transition: all 0.3s;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+export const NavigationButton = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 24px;
+  cursor: pointer;
+
+  &:focus {
+    outline: none; /* Usuń dekoracje (outline) w stanie focus */
+  }
+`;
+
+export const EnlargedImage = styled.img`
+  display: block;
+  margin: auto;
+  max-width: 100%;
+  max-height: 90vh;
+  object-fit: contain;
+  border-radius: 10px; 
+  transition: transform 0.3s ease-in-out;
+  transform-origin: 50% 50%;
+  transition: all 0.3s;
+`;
